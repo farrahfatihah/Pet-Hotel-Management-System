4dea237a42e38b9b272135849ef3f1287f9529fe
farrahfatihah <frrahfatihah@gmail.com>
2025-05-10 15:19:02 +0800

    Update booking and room class

--- src/PetHotelManagement/Booking.java
+++ src/PetHotelManagement/Booking.java
@@ -3,11 +3,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package PetHotelManagement;
-
+import java.util.Date;
 /**
  *
  * @author frrah
  */
 public class Booking {
+    private String bookingId;
+    private Room room;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private Payment payment;
+    private double ratePerNight = 30.0;
     
+    public Booking(String bookingId, Room room, Date checkInDate, Date checkOutDate) {
+        this.bookingId = bookingId;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+    
+    public String getBookingId() {
+        return bookingId;
+    }
+    
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+    
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+    
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+    
+    public Date setCheckOutDate() {
+        return checkOutDate;
+    }
+    
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+    
+    public double getTotalCost() {
+        long milliseconds = checkOutDate.getTime() - checkInDate.getTime();
+        long days = milliseconds / (1000 * 60 * 60 * 24);
+        return days * ratePerNight;
+    }
+    
+    public Payment getPayment() {
+        return payment;
+    }
+    
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
--- src/PetHotelManagement/Room.java
+++ src/PetHotelManagement/Room.java
@@ -9,5 +9,22 @@ package PetHotelManagement;
  * @author frrah
  */
 public class Room {
+    private String roomId;
+    private boolean available;
     
+    public String getRoomId() {
+        return roomId;
+    }
+    
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+    
+    public boolean getAvailable() {
+        return available;
+    }
+    
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
