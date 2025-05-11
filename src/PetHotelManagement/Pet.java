5c36d229203619d023386a646c5bfede5d5bac57
nkhaisofea <khairinasofea05@gmail.com>
2025-05-11 12:08:40 +0800

    update on Pet class, PIC can go through if have any errors

--- src/PetHotelManagement/Pet.java
+++ src/PetHotelManagement/Pet.java
@@ -9,5 +9,28 @@ package PetHotelManagement;
  * @author frrah
  */
 public class Pet {
+    private String petId;
+    private String petName;
+    
+    public Pet(String petId, String petName) {
+        this.petId = petId;
+        this.petName = petName;
+    }
+    
+    public String getPetId() {
+    return petId;
+    }
+
+    public void setPetId(String petId) {
+    this.petId = petId;
+    }
+
+    public String getPetName() {
+    return petName;
+    }
+
+    public void setPetName(String petName) {
+    this.petName = petName;
+    }
     
 }
