8e48555000eca101e77d9ca32a7c7644590d0659
nkhaisofea <khairinasofea05@gmail.com>
2025-05-11 12:04:57 +0800

    update on Payment class, PIC can go through if have any errors

--- src/PetHotelManagement/Payment.java
+++ src/PetHotelManagement/Payment.java
@@ -3,11 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package PetHotelManagement;
+import java.util.Date;
 
 /**
  *
  * @author frrah
  */
 public class Payment {
+    private String paymentId;
+    private boolean hasPaid;
+    private double amount;
+    private Date date;
     
+    public Payment(String paymentId, boolean hasPaid, double amount, Date date) {
+        this.paymentId = paymentId;
+        this.hasPaid = hasPaid;
+        this.amount = amount;
+        this.date = date;
+    }
+    
+    public String getPaymentId() {
+    return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+    this.paymentId = paymentId;
+    }
+
+    public boolean getHasPaid() {
+    return hasPaid;
+    }
+
+    public void setHasPaid(boolean hasPaid) {
+    this.hasPaid = hasPaid;
+    }
+
+    public double getAmount() {
+    return amount;
+    }
+
+    public void setAmount(double amount) {
+    this.amount = amount;
+    }
+
+    public Date getDate() {
+    return date;
+    }
+
+    public void setDate(Date date) {
+    this.date = date;
+    } 
 }
